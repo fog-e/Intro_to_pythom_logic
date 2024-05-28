6120f3ead6dca759a8716085b520afaef7ab7298
fog-e <165981269+fog-e@users.noreply.github.com>
2024-05-28 20:33:33 +0000

    Randomize first letter of each name in for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -25,3 +25,38 @@ names = ["Kellyn", "Max", "Joe", "Roan"]
 
 for name in names:
     print(name + "@themultiverse.school")
+import random
+import string
+
+# Function to randomize the first letter of a name
+def randomize_first_letter(name):
+    first_letter = random.choice(string.ascii_uppercase)
+    return first_letter + name[1:]
+
+# List of guest names
+guests = "Ash,Max,Joe,Roan,Caitlin"
+
+# Split the guest names into a list
+names = guests.split(",")
+
+# Append additional names
+names.append("Liz")
+names.append("Liz")
+
+# Print the length of the names list and the first name
+print(len(names))
+print(names[0])
+
+# Change the name at index 6 to "Kellyn"
+names[6] = "Kellyn"
+
+# Randomize the first letter of each name in the names list
+randomized_names = [randomize_first_letter(name) for name in names]
+
+# Print the name that was popped from the list
+first_person = names.pop(0)
+print(first_person)
+
+# Print each name with the domain appended
+for name in randomized_names:
+    print(name + "@themultiverse.school")
