45839079120ecc14e55a5b2a56c707c06673a281
fog-e <165981269+fog-e@users.noreply.github.com>
2024-05-28 17:43:50 +0000

    Added password validation logic

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # omtro_to_pythom_logic
 Cultural and data structures
+
+# intro_to_python_logic
+
+Control Structures and Data Structures, oh my!
+
+Today:
+- If Statements
+- Comparison Operations
+- Boolean Logic
+- For Loops
+- While Loops
+
+This Afternoon
+- Arrays
+- Dictionaries
+- String Splitting and Joining
+
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-password = input("Please set a new password")
+valid_password = False
 
-special_character_check = "!" in password
+# until we get a valid password
+while not valid_password:
+    password = input("Please set a new password")
+    
+    length_valid = False
+    special_character_valid = False
+    
+    if len(password) < 16 and len(password) > 5:
+        print("password is the right length")
+        length_valid = True
+    elif len(password) <= 5:
+        print("Password is too short")
+    else:
+        print("password must be between 5 and 16 characters")
+    
+    if "!" in password:
+        print("special character found")
+        special_character_valid = True
+    else:
+        print("This password needs an ! character somewhere")
+    
+    if special_character_valid and length_valid:
+        print("Your password has been set")
+        valid_password = True
 
-if len(password) < 16 and len(password) > 5:
-    print("password is the right length")
-elif len(password) < 5:
-    print("Password is too short")
-else:
-    print("password must be between 5 and 16 characters")
-
-if special_character_check:
-    print("special character found")
-else:
-    print("This password needs an ! character somewhere")
+print("Thank you for resetting your password")
